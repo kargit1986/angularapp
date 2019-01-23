3565a294be3e3ff0ef5acffd2961b959832d264a
=k <k@k.com>
2019-01-23 12:36:51 +0400

    date

--- src/app/hello-world/hello-world.component.spec.ts
+++ src/app/hello-world/hello-world.component.spec.ts
@@ -6,20 +6,4 @@ describe('HelloWorldComponent', () => {
   let component: HelloWorldComponent;
   let fixture: ComponentFixture<HelloWorldComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HelloWorldComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HelloWorldComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
